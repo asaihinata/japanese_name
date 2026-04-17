20f6cd7f4dbee30e1389daa568bed99ad527f82a
asaihinata <hinataasaisacount@gmail.com>
2026-04-18 08:47:02 +0900

    refactor
    refactor
    __all__の追加
    __name__=="__main__"の追加

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import csv
 import os
 import random
 from pathlib import Path
+__all__=['Japansename','Manname','Womanname']
 Data_file_path=os.path.join(Path(__file__).parent,"data")
 Family_name=os.path.join(Data_file_path,"japanese_family_name.csv")
 def getcsv(path):
@@ -39,3 +40,13 @@ class Womanname:
         self.nameR=f"{self.familyname[3]} {self.data[3]}"
     def __iter__(self):return iter([self.name,self.namek,self.nameh,self.nameR])
     def __str__(self):return self.name
+if __name__=="__main__":
+    japanese_name=Japansename()
+    man_name=Manname()
+    woman_name=Womanname()
+    print(str(japanese_name))
+    print(list(japanese_name))
+    print(str(man_name))
+    print(list(man_name))
+    print(str(woman_name))
+    print(list(woman_name))
